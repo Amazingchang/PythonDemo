0cfb3cb702d29fc6fe771b716bfcd566a8959b9b
amazingchang <205538406@qq.com>
2017-07-27 15:44:06 +0800

    15点44分

--- list.py
+++ list.py
@@ -35,3 +35,30 @@ for index in enumerate(L):
 
 for index in enumerate(L):
     print index[1]
+
+#列表生成式
+print [x * x for x in range(1,11)]
+
+print range(1, 100, 2)
+
+print [x * (x+1) for x in range(1,100,2)]
+#条件过滤
+print [x * x for x in range(1,11) if x % 2 == 0]
+
+
+def toUppers(L):
+    return [ x.upper() for x in L if isinstance(x,str)]
+
+print toUppers(['Hello', 'world', 101])
+
+
+def toUppers1(L):
+    for item in enumerate(L):
+        if isinstance(item[1],str):
+            L[item[0]] = item[1].upper()
+        else:
+            L.remove(item[1])
+    return L
+print toUppers1(['Hello', 'world', 101])
+
+print [x for x in range(100,1000) if x/100 == x%10]
